3345b20979bc145149270d291b9e848ff22290e7
Aaron <aaron.harper@outlook.com>
2019-11-23 15:11:43 -0500

    Cleanup subnet-per-az

--- subnet-per-az/network.tf
+++ subnet-per-az/network.tf
@@ -1,32 +1,34 @@
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
+  enable_dns_hostnames = true
+
   tags = {
     Name = "main"
   }
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  count                   = "${length(data.aws_availability_zones.all.names)}"
+  # In each AZ, but no more than 3 AZs.
+  count                   = "${min(length(data.aws_availability_zones.all.names), 3)}"
+
   availability_zone       = "${element(data.aws_availability_zones.all.names, count.index)}"
   cidr_block              = "10.0.${count.index + 1}.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = true
+  vpc_id                  = "${aws_vpc.main.id}"
 
   tags = {
-    Name = "public-${count.index}"
+    Name = "public-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  count                   = "${length(data.aws_availability_zones.all.names)}"
+  # In each AZ, but no more than 3 AZs.
+  count                   = "${min(length(data.aws_availability_zones.all.names), 3)}"
+
   availability_zone       = "${element(data.aws_availability_zones.all.names, count.index)}"
   cidr_block              = "10.0.${length(aws_subnet.public) + count.index + 1}.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = true
+  vpc_id                  = "${aws_vpc.main.id}"
 
   tags = {
     Name = "private-${count.index + 1}"

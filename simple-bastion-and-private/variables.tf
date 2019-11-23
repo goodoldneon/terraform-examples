badfa5c27a8a6f981d10a6d57bb42adb31a08939
Aaron <aaron.harper@outlook.com>
2019-11-23 15:12:27 -0500

    Cleanup simple-bastion-and-private

--- simple-bastion-and-private/variables.tf
+++ simple-bastion-and-private/variables.tf
@@ -1,18 +1,22 @@
 variable "profile" {
+  type    = "string"
   default = "default"
 }
 
 variable "region" {
+  type    = "string"
   default = "us-east-1"
 }
 
 variable "ubuntu_amis" {
   type = "map"
+
   default = {
     us-east-1 = "ami-b3425cc9"
   }
 }
 
 variable "key_path" {
+  type    = "string"
   default = "~/.ssh/id_rsa.pub"
 }
